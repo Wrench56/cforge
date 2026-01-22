69733b13e3717fea1859ea3fb138e3d9885ba514
Mark Devenyi <markdevenyidev@gmail.com>
2026-01-22 14:59:42 -0500

    feat(configs): add basic config parsing

--- cforge.h
+++ cforge.h
@@ -13,6 +13,7 @@ exit 0
 #include <string.h>
 
 #define CF_MAX_TARGETS 64
+#define CF_MAX_CONFIGS 64
 
 #define CF_ERR_LOG(...) fprintf(stderr, __VA_ARGS__)
 #define CF_WRN_LOG(...) fprintf(stdout, __VA_ARGS__)
@@ -20,8 +21,11 @@ exit 0
 #define CF_SUCCESS_EC 0
 #define CF_MAX_TARGETS_EC 1
 #define CF_TARGET_NOT_FOUND_EC 2
+#define CF_MAX_CONFIGS_EC 3
+#define CF_INVALID_STATE_EC 4
 
 typedef uint32_t (*cf_target_fn)(uint32_t argc, char** argv);
+typedef void (*cf_config_fn)(void);
 
 typedef struct {
     const char* name;
@@ -29,18 +33,57 @@ typedef struct {
     bool executed;
 } cf_target_decl_t;
 
+typedef struct {
+    const char* name;
+    cf_config_fn fn;
+} cf_config_decl_t;
+
+typedef enum {
+    REGISTER_PHASE = 0,
+    CONFIG_CONSTRUCT_PHASE = 1,
+    TARGET_CONSTRUCT_PHASE = 2
+} cf_state_t;
+
 static cf_target_decl_t cf_targets[CF_MAX_TARGETS] = { 0 };
 static size_t cf_num_targets = 0;
 
+static cf_config_decl_t cf_configs[CF_MAX_CONFIGS] = { 0 };
+static size_t cf_num_configs = 0;
+
+static cf_state_t cf_state = REGISTER_PHASE;
+
 static void cf_register_target(const char* name, cf_target_fn fn) {
+    if (cf_state != REGISTER_PHASE) {
+        CF_ERR_LOG("Error: Invalid cf_state (%d) when registering target!\n", cf_state);
+        exit(CF_INVALID_STATE_EC);
+    }
+
     if (cf_num_targets >= CF_MAX_TARGETS) {
         CF_ERR_LOG("Error: Maximum targets of %d was reached!\n", CF_MAX_TARGETS);
-        exit(CF_TARGET_NOT_FOUND_EC);
+        exit(CF_MAX_TARGETS_EC);
     }
+
     cf_targets[cf_num_targets++] = (cf_target_decl_t) {
         .name = name,
         .fn = fn,
         .executed = false
+    };
+}
+
+static void cf_register_config(const char* name, cf_config_fn fn) {
+    if (cf_state != REGISTER_PHASE) {
+        CF_ERR_LOG("Error: Invalid cf_state (%d) when registering config!\n", cf_state);
+        exit(CF_INVALID_STATE_EC);
+    }
+
+    if (cf_num_targets >= CF_MAX_TARGETS) {
+        CF_ERR_LOG("Error: Maximum configs of %d was reached!\n", CF_MAX_CONFIGS);
+        exit(CF_MAX_CONFIGS_EC);
+    }
+
+    cf_configs[cf_num_configs++] = (cf_config_decl_t) {
+        .name = name,
+        .fn = fn
     };
 }
 
@@ -51,18 +94,36 @@ static void cf_register_target(const char* name, cf_target_fn fn) {
     } \
     static uint32_t cf_target_##name_ident(uint32_t argc, char** argv)
 
+
+#define CF_CONFIG(name_ident) \
+    static void cf_config_##name_ident(void); \
+    __attribute__((constructor)) static void cf_config_reg_##name_ident(void) { \
+        cf_register_config(#name_ident, cf_config_##name_ident); \
+    } \
+    static void cf_config_##name_ident(void)
+
+
 #define CF_FINISH(retval) \
     (void) argc; \
     (void) argv; \
     return retval;
 
 __attribute__((weak)) int main(int argc, char** argv) {
+    (void) cf_register_config;
+    (void) cf_register_target;
+
     if (argc == 1) {
         return CF_SUCCESS_EC;
     }
 
+    cf_state = CONFIG_CONSTRUCT_PHASE;
+    for (size_t i = 0; i < cf_num_configs; i++) {
+        cf_configs[i].fn();
+    }
+
+    cf_state = TARGET_CONSTRUCT_PHASE;
     for (int32_t i = 1; i < argc; i++) {
-        for (uint32_t j = 0; j < cf_num_targets; j++) {
+        for (size_t j = 0; j < cf_num_targets; j++) {
             cf_target_decl_t* target = &cf_targets[j];
             if (strcmp(target->name, argv[i]) == 0) {
                 if (target->executed) {
