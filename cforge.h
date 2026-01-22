9b12b9115aeca9c7b3585a34f462065d3640d9e7
Mark Devenyi <markdevenyidev@gmail.com>
2026-01-22 09:01:23 -0500

    feat(target): warn on duplicate target runs

--- cforge.h
+++ cforge.h
@@ -15,6 +15,7 @@ exit 0
 #define CF_MAX_TARGETS 64
 
 #define CF_ERR_LOG(...) fprintf(stderr, __VA_ARGS__)
+#define CF_WRN_LOG(...) fprintf(stdout, __VA_ARGS__)
 
 #define CF_SUCCESS_EC 0
 #define CF_MAX_TARGETS_EC 1
@@ -25,6 +26,7 @@ typedef uint32_t (*cf_target_fn)(uint32_t argc, char** argv);
 typedef struct {
     const char* name;
     cf_target_fn fn;
+    bool executed;
 } cf_target_decl_t;
 
 static cf_target_decl_t cf_targets[CF_MAX_TARGETS] = { 0 };
@@ -35,7 +37,11 @@ static void cf_register_target(const char* name, cf_target_fn fn) {
         CF_ERR_LOG("Error: Maximum targets of %d was reached!\n", CF_MAX_TARGETS);
         exit(CF_TARGET_NOT_FOUND_EC);
     }
-    cf_targets[cf_num_targets++] = (cf_target_decl_t) { .name = name, .fn = fn };
+    cf_targets[cf_num_targets++] = (cf_target_decl_t) {
+        .name = name,
+        .fn = fn,
+        .executed = false
+    };
 }
 
 #define CF_TARGET(name_ident) \
@@ -57,9 +63,14 @@ __attribute__((weak)) int main(int argc, char** argv) {
 
     for (int32_t i = 1; i < argc; i++) {
         for (uint32_t j = 0; j < cf_num_targets; j++) {
-            cf_target_decl_t target = cf_targets[j];
-            if (strcmp(target.name, argv[i]) == 0) {
-                target.fn(0, NULL);
+            cf_target_decl_t* target = &cf_targets[j];
+            if (strcmp(target->name, argv[i]) == 0) {
+                if (target->executed) {
+                    CF_WRN_LOG("Warning: Target \"%s\" was executed already! Skipping target...\n", argv[i]);
+                    goto next_iter;
+                }
+                target->executed = true;
+                target->fn(0, NULL);
                 goto next_iter;
             }
         }
